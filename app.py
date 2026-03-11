e4bac1d4418e169341cf5c9abb760bb65ce46692
idc636 <nigite636@gmail.com>
2026-03-12 03:26:36 +0600

    Update app.py

--- app.py
+++ app.py
@@ -386,7 +386,9 @@ ALLOWED_FIELDS = {
     'temp_outdoor', 'temp_supply', 'temp_return',
     'temp_graph_supply', 'temp_graph_return',
     'pressure_supply', 'pressure_return',
-    'water_consumption_daily', 'staff_night', 'staff_day', 'notes','downtime_today'
+    'water_consumption_daily', 'staff_night', 'staff_day', 'notes',
+    'downtime_today', 'downtime_total',  # ← добавили
+    'boiler_location', 'boiler_contact', 'burner_model'  # ← добавили
 }
 
 @app.route('/update', methods=['POST'])
